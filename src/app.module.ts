a046d096e097d1b28d675632296fbd253345b442
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-11-22 22:42:54 +0100

    html

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { AppRegisterModule } from './app-register/app-register.module';
 import { APP_PIPE } from '@nestjs/core';
 import { ZodValidationPipe } from 'nestjs-zod';
 import { AplModule } from './apl/apl.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [AppManifestModule, AppRegisterModule, AplModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [
     {
       provide: APP_PIPE,
